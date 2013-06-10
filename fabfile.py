b85ecbd18250cd94989fe4b3c803a2ebf54dac3b
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-06-10 18:34:08 +0200

    Update fabfile configuration

--- fabfile.py
+++ fabfile.py
@@ -1,39 +1,47 @@
-from __future__ import with_statement
+from fabric.api import task
 from fabric.api import cd
 from fabric.api import env
-from fabric.api import local
 from fabric.api import run
-from fabric.api import sudo
 
+from ade25.fabfiles import project
 
-def deployment():
-    env.use_ssh_config = True
-    env.forward_agent = True
-    env.port = '22222'
-    env.user = 'root'
-    env.hosts = ['sechszuvier.vorwaerts-werbung.de']
-    env.webserver = '/opt/webserver/buildout.webserver'
-    env.code_root = '/opt/sites/re/buildout.re'
-    env.code_user = 'root'
-    env.prod_user = 'www'
-
-
-def ls():
-    with cd(env.code_root):
-        run('ls')
-
-
-def update():
-    with cd(env.code_root):
-        run('nice git pull')
-
-
-def rebuild():
-    with cd(env.code_root):
-        run('bin/buildout -Nc deployment.cfg')
+env.use_ssh_config = True
+env.forward_agent = True
+env.port = '22222'
+env.user = 'root'
+env.hosts = ['6zu4']
+env.webserver = '/opt/webserver/buildout.webserver'
+env.code_root = '/opt/sites/buildout.re'
+env.local_root = '/Users/cb/dev/erben/buildout.re'
+env.sitename = 'plonesite'
+env.code_user = 'root'
+env.prod_user = 'www'
 
 
 def supervisorctl(*cmd):
     """Runs an arbitrary supervisorctl command."""
     with cd(env.webserver):
-        run('bin/supervisorctl ' + ' '.join(cmd), user=env.prod_user)
+        run('bin/supervisorctl ' + ' '.join(cmd))
+
+
+@task
+def deploy():
+    """ Deploy current master to production server """
+    project.site.update()
+    project.site.estart()
+
+
+@task
+def deploy_full():
+    """ Deploy current master to production and run buildout """
+    project.site.update()
+    project.site.build()
+    project.site.restart()
+
+
+@task
+def rebuild():
+    """ Deploy current master and run full buildout """
+    project.site.update()
+    project.site.build_full()
+    project.site.restart()
