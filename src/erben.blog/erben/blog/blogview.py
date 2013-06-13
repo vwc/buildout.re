49ad40afbfbe9648db9a857a95e7c17851d123f5
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-06-13 12:28:42 +0200

    Add blog category filter view and restyle the forntend

--- src/erben.blog/erben/blog/blogview.py
+++ src/erben.blog/erben/blog/blogview.py
@@ -69,6 +69,59 @@ class BlogView(grok.View):
         return IContentListing(results)
 
 
+class BlogCategoryView(grok.View):
+    grok.context(IFolderish)
+    grok.require('zope2.View')
+    grok.name('blog-category-view')
+
+    def blogitems(self):
+        """List all blog items as brains"""
+        year = int(self.request.form.get('year', 0))
+        month = int(self.request.form.get('month', 0))
+        subject = self.request.form.get('category', None)
+        return self.get_entries(year=year, month=month, subject=subject)
+
+    def batch(self):
+        b_size = 5
+        b_start = self.request.form.get('b_start', 0)
+        return Batch(self.blogitems(), b_size, b_start, orphan=1)
+
+    def commentsEnabled(self, ob):
+        conversation = IConversation(ob)
+        return conversation.enabled()
+
+    def commentCount(self, ob):
+        conversation = IConversation(ob)
+        return len(conversation)
+
+    def _base_query(self):
+        context = aq_inner(self.context)
+        obj_provides = IBlogEntry.__identifier__
+        path = '/'.join(context.getPhysicalPath())
+        return dict(path={'query': path, 'depth': 2},
+                    object_provides=obj_provides,
+                    sort_on='effective', sort_order='reverse')
+
+    def get_entries(self, year=None, month=None, subject=None):
+        context = aq_inner(self.context)
+        catalog = getToolByName(context, 'portal_catalog')
+        query = self._base_query()
+        if subject:
+            query['Subject'] = subject
+        if year:
+            if month:
+                lastday = calendar.monthrange(year, month)[1]
+                startdate = DateTime(year, month, 1, 0, 0)
+                enddate = DateTime(year, month, lastday, 23, 59, 59)
+            else:
+                startdate = DateTime(year, 1, 1, 0, 0)
+                enddate = DateTime(year, 12, 31, 0, 0)
+            query['effective'] = dict(query=(startdate, enddate),
+                                      range='minmax')
+        results = catalog.searchResults(**query)[:5]
+        return IContentListing(results)
+
+
 class PressView(grok.View):
     grok.context(IFolderish)
     grok.require('zope2.View')
