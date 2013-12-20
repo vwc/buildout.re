bc8d93759d9f0db7b2b68865cca6887807cb9dbf
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-12-20 18:44:20 +0100

    Refactor the theme layout and modernize

---
+++ src/erben.sitetheme/erben/sitetheme/resources/Gruntfile.js
@@ -0,0 +1,256 @@
+/* jshint node: true */
+'use strict';
+
+module.exports = function (grunt) {
+
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    // Project configuration.
+    grunt.initConfig({
+
+        // Metadata.
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*!\n' +
+                  '* RE v<%= pkg.version %> by VWC\n' +
+                  '* Copyright <%= pkg.author %>\n' +
+                  '* Licensed under <%= pkg.licenses %>.\n' +
+                  '*\n' +
+                  '* Designed and built by ade25\n' +
+                  '*/\n',
+        jqueryCheck: 'if (typeof jQuery === "undefined") { throw new Error("Bootstrap requires jQuery") }\n\n',
+
+        // Task configuration.
+        clean: {
+            dist: ['dist']
+        },
+
+        jshint: {
+            options: {
+                jshintrc: 'js/.jshintrc'
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            src: {
+                src: ['js/*.js']
+            },
+            test: {
+                src: ['js/tests/unit/*.js']
+            }
+        },
+
+        concat: {
+            options: {
+                banner: '<%= banner %><%= jqueryCheck %>',
+                stripBanners: false
+            },
+            dist: {
+                src: [
+                    'bower_components/jquery/jquery.js',
+                    'bower_components/modernizr/modernizr.js',
+                    'bower_components/bootstrap/dist/js/bootstrap.js',
+                    'js/application.js'
+                ],
+                dest: 'dist/js/<%= pkg.name %>.js'
+            },
+            theme: {
+                src: [
+                    'bower_components/bootstrap/dist/js/bootstrap.js',
+                    'js/application.js'
+                ],
+                dest: 'dist/js/application.js'
+            }
+        },
+
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            dist: {
+                src: ['<%= concat.dist.dest %>'],
+                dest: 'dist/js/<%= pkg.name %>.min.js'
+            }
+        },
+
+        recess: {
+            options: {
+                compile: true
+            },
+            theme: {
+                src: ['less/styles.less'],
+                dest: 'dist/css/styles.css'
+            },
+            min: {
+                options: {
+                    compress: true
+                },
+                src: ['less/styles.less'],
+                dest: 'dist/css/styles.min.css'
+            }
+        },
+
+        copy: {
+            fonts: {
+                expand: true,
+                flatten: true,
+                cwd: 'bower_components/',
+                src: ['font-awesome/fonts/*'],
+                dest: 'dist/assets/fonts/'
+            },
+            ico: {
+                expand: true,
+                flatten: true,
+                src: ['assets/ico/*'],
+                dest: 'dist/assets/ico/'
+            }
+        },
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'assets/img/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'dist/assets/img/'
+                }]
+            }
+        },
+
+        rev: {
+            options:  {
+                algorithm: 'sha256',
+                length: 8
+            },
+            files: {
+                src: ['dist/**/*.{js,css,png,jpg}']
+            }
+        },
+        qunit: {
+            options: {
+                inject: 'js/tests/unit/phantom.js'
+            },
+            files: ['js/tests/*.html']
+        },
+
+        connect: {
+            server: {
+                options: {
+                    port: 3000,
+                    base: '.'
+                }
+            }
+        },
+        jekyll: {
+            theme: {}
+        },
+
+        sed: {
+            'clean-source-assets': {
+                path: 'dist/',
+                pattern: '../../assets/',
+                replacement: '../assets/',
+                recursive: true
+            },
+            'clean-source-css': {
+                path: 'dist/',
+                pattern: '../dist/css/styles.css',
+                replacement: 'css/styles.css',
+                recursive: true
+            },
+            'clean-logo': {
+                path: 'dist/',
+                pattern: '../assets/img/badge.png',
+                replacement: '/++theme++erben.sitetheme/assets/img/badge.png'
+            }
+        },
+
+        validation: {
+            options: {
+                reset: true
+            },
+            files: {
+                src: ['_site/**/*.html']
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['js/*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    spawn: false
+                }
+            },
+            recess: {
+                files: 'less/*.less',
+                tasks: ['recess'],
+                options: {
+                    spawn: false
+                }
+            },
+            templates: {
+                files: '*.html',
+                tasks: ['jekyll:theme']
+            }
+        }
+    });
+
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-html-validation');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-newer');
+    grunt.loadNpmTasks('grunt-jekyll');
+    grunt.loadNpmTasks('grunt-recess');
+    grunt.loadNpmTasks('grunt-sed');
+    grunt.loadNpmTasks('grunt-rev');
+
+    // Prepare distrubution
+    grunt.registerTask('dist-init', '', function () {
+        grunt.file.mkdir('dist/assets/');
+    });
+
+    // Copy jekyll generated templates and rename for diazo
+    grunt.registerTask('copy-templates', '', function () {
+        grunt.file.copy('_site/index.html', 'dist/theme.html');
+    });
+
+    // Docs HTML validation task
+    grunt.registerTask('validate-html', ['jekyll', 'validation']);
+
+    // Test task.
+    var testSubtasks = ['dist-css', 'jshint', 'qunit', 'validate-html'];
+
+    grunt.registerTask('test', testSubtasks);
+
+    // JS distribution task.
+    grunt.registerTask('dist-js', ['concat', 'newer:uglify']);
+
+    // CSS distribution task.
+    grunt.registerTask('dist-css', ['recess']);
+
+    // Assets distribution task.
+    grunt.registerTask('dist-assets', ['newer:copy', 'newer:imagemin']);
+
+    // Cache buster distribution task.
+    grunt.registerTask('dist-cb', ['rev']);
+
+    // Template distribution task.
+    grunt.registerTask('dist-html', ['jekyll:theme', 'copy-templates', 'newer:sed']);
+
+    // Full distribution task.
+    grunt.registerTask('dist', ['clean', 'dist-css', 'dist-js', 'dist-html', 'dist-assets']);
+
+    // Default task.
+    grunt.registerTask('default', ['test', 'dist']);
+};
